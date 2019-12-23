08a76c66e06422021a42956ab6e9ffa94bd8a122
Juraj Borza (via Travis CI) <jborza@gmail.com>
2019-12-23 10:02:30 +0000

    Deploy jborza/elasticsearch-taskview to github.com/jborza/elasticsearch-taskview.git:gh-pages

---
+++ precache-manifest.3f03fda0d9990eba5add68652ad6a776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec0f5f4bed07dd3b959cf93b9dcbac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b6f7b2ed25c3c27203",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1b099cef85fccba53d43",
+    "url": "/static/js/2.ed0e0646.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ed0e0646.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72b6f7b2ed25c3c27203",
+    "url": "/static/js/main.b2e02e40.chunk.js"
+  },
+  {
+    "revision": "14f30d6798adfa325c02",
+    "url": "/static/js/runtime-main.37131bb7.js"
+  }
+]);
--- src/truncate.js
+++
@@ -1,2 +0,0 @@
-const truncate = (input, max_length) => input.length > max_length ? `${input.substring(0, max_length)}...` : input;
-export {truncate as default};
