eebbd080a55c8b95a5fd2441d378850f5514f86f
jborza (via Travis CI) <jborza@gmail.com>
2019-12-29 19:17:02 +0000

    Deploy jborza/elasticsearch-taskview to github.com/jborza/elasticsearch-taskview.git:gh-pages

---
+++ precache-manifest.091c9b3b5173059fdc60cc0885dfe2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0018b5d2f3860fb51e3a6d57017bb93",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1722ed3eef966f033a54",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1b099cef85fccba53d43",
+    "url": "./static/js/2.ed0e0646.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.ed0e0646.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1722ed3eef966f033a54",
+    "url": "./static/js/main.8839ae1c.chunk.js"
+  },
+  {
+    "revision": "7b12e2eeb454690985b7",
+    "url": "./static/js/runtime-main.eba019c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f03fda0d9990eba5add68652ad6a776.js"
+  "./precache-manifest.091c9b3b5173059fdc60cc0885dfe2b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
